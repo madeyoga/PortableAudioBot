4d57ab6232d6857b42796cbe5b3c7292baf54c45
MadeYoga <vngla21@gmail.com>
2021-10-04 14:29:07 +0700

    Add command cooldown feature

--- src/main/java/interactions/Command.java
+++ src/main/java/interactions/Command.java
@@ -3,8 +3,20 @@ package interactions;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
+import java.util.Map;
+
 public abstract class Command implements ICommand {
     protected boolean guildOnly = true;
+
+    /**
+     * Command cooldown in seconds
+     */
+    protected float cooldown = 0;
+
+    /**
+     * User id as key and time in millis as value (Cool down time ends)
+     */
+    protected Map<String, Long> cooldownMap;
     protected CommandData commandData;
     protected CommandCategory category;
 
@@ -28,5 +40,13 @@ public abstract class Command implements ICommand {
     @Override
     public CommandCategory getCategory() {
         return category;
+    }
+
+    public Map<String, Long> getCooldownMap() {
+        return cooldownMap;
+    }
+
+    public float getCooldown() {
+        return cooldown;
     }
 }
